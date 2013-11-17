91217e90ac20e9b05cc2d43a6845140e7b3314dc
zhixing <zhixing135@hotmail.com>
2013-11-18 03:23:15 +0800

    Changed NormalizedSimilarity

--- src/cs3246/a2/NormalizedSimilarity.java
+++ src/cs3246/a2/NormalizedSimilarity.java
@@ -6,15 +6,22 @@ public class NormalizedSimilarity extends Similarity{
 	double compute(double[] query, double[] document) {
 		
 		double Sum = 0.0; 
+		double querySqr = 0.0;
+		double docSqr = 0.0;
+		
         for(int i = 0; i < query.length; i++) { 
-        	double distance = Math.abs(query[i] - document[i]);
-        	double max = query[i] > document[i] ? query[i] : document[i];
-        	if (max == 0){
-        		continue;
-        	}
-        	Sum += (query[i] * ( 1 - distance / max)); 
+//        	double distance = Math.abs(query[i] - document[i]);
+//        	double max = query[i] > document[i] ? query[i] : document[i];
+//        	if (max == 0){
+//        		continue;
+//        	}
+//        	Sum += (query[i] * ( 1 - distance / max)); 
+        	
+        	Sum += query[i] * document[i];
+        	querySqr += query[i] * query[i];
+        	docSqr += document[i] * document[i];
         } 
-        return Sum; 
+        return Sum / Math.sqrt(docSqr) / Math.sqrt(querySqr); 
         
 	}
 
