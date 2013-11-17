5245ce13bc3ba50399402afe65336a48c02df538
jasonpeng <jasonpeng.boy@gmail.com>
2013-11-17 21:13:01 +0800

    updated edge computation

--- src/cs3246/a2/SobelOperator.java
+++ src/cs3246/a2/SobelOperator.java
@@ -3,6 +3,8 @@ package cs3246.a2;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Sobel operator for edge detection
@@ -15,16 +17,42 @@ public class SobelOperator implements FeatureExtractor {
 	private BufferedImage mImage;
 	private int mSizeX;
 	private int mSizeY;
-	private double[][] mGradient;
-	private double mGradientMax;
-	private double[][] mDirection;
+	
+	private double[][] mMatrix;
 	private double[] mFeature;
 
-	private static final int[][] KERNEL_X = new int[][] { { 1, 0, -1 },
-			{ 2, 0, -2 }, { 1, 0, -1 } };
+	private static final double GAUSSIAN_COEFFICIENT = 1.0f / 159.0f;
+	private static final int[][] GAUSSIAN_5x5 = new int[][] {
+		{2, 4, 5, 4, 2},
+		{4, 9, 12, 9, 4},
+		{5, 12, 15, 12, 5},
+		{4, 9, 12, 9, 4},
+		{2, 4, 5, 4, 2}
+	};
+	
+	private static final int[][] KERNEL_0 = new int[][] { 
+		{ 1, 0, -1 },
+		{ 2, 0, -2 },
+		{ 1, 0, -1 }
+	};
+	
+	private static final int[][] KERNEL_1 = new int[][] {
+		{ 0, -1, -2 },
+		{ 1,  0, -1 },
+		{ 2,  1,  0 }
+	};
 
-	private static final int[][] KERNEL_Y = new int[][] { { 1, 2, 1 },
-			{ 0, 0, 0 }, { -1, -2, -1 } };
+	private static final int[][] KERNEL_2 = new int[][] { 
+		{ -1, -2, -1 },
+		{  0,  0,  0 },
+		{  1,  2,  1 }
+	};
+	
+	private static final int[][] KERNEL_3 = new int[][] { 
+		{ -2, -1,  0 },
+		{ -1,  0,  1 },
+		{  0,  1,  2 }
+	};
 
 	/**
 	 * @param image
@@ -33,84 +61,97 @@ public class SobelOperator implements FeatureExtractor {
 	public SobelOperator() {
 
 	}
+	
+	/** 
+	 * blur image to reduce noise
+	 */
+	public void applyGaussianFilter() {
+		int x, y;
+		double[][] gaussian = new double[mSizeX][mSizeY];
+		BufferedImage YCbCrImage = Util.RGBToYCbCr(mImage);
+		Raster YCbCrRaster = YCbCrImage.getRaster();
+		
+		for (x=2; x<mSizeX-3; x++) {
+			for (y=2; y<mSizeY-3; y++) {
+				double sum = 0;
+				
+				for (int i=-2; i<=2; i++) {
+					for (int j=-2; j<2; j++) {
+						sum += GAUSSIAN_5x5[i+2][j+2] * YCbCrRaster.getSample(x + i, y + j, 0);
+					}
+				}
+				gaussian[x][y] = sum / GAUSSIAN_COEFFICIENT;
+			}
+		}
+		
+		mMatrix = gaussian;
+	}
+	
 
 	/**
-	 * computes the edge gradients using KERNEL_X and KERNEL_Y
+	 * computes the edge gradients on Y channel using kernels
 	 * 
 	 * @return
 	 */
 	public void compute() {
-		mGradient = new double[mSizeX][mSizeY];
-		mDirection = new double[mSizeX][mSizeY];
-		int gx, gy;
 		int x, y;
-		BufferedImage YCbCrImage = Util.RGBToYCbCr(mImage);
-		Raster YCbCrRaster = YCbCrImage.getRaster();
-
-		mGradientMax = 0;
 		
-		// ignore border pixels
-		for (x = 1; x < mSizeX - 1; x++) {
-			for (y = 1; y < mSizeY - 1; y++) {
-				// compute gx and gy for each pixel in Y channel (band 0)
-				gx = gy = 0;
-				for (int i = -1; i <= 1; i++) {
-					for (int j = -1; j <= 1; j++) {
-						gx += KERNEL_X[i + 1][j + 1]
-								* YCbCrRaster.getSample(x + i, y + j, 0);
-						gy += KERNEL_Y[i + 1][j + 1]
-								* YCbCrRaster.getSample(x + i, y + j, 0);
-					}
-				}
-
-				// compute gradient and direction
-				mGradient[x][y] = Math.sqrt(gx * gx + gy * gy);
-				if (mGradient[x][y] > mGradientMax) {
-					mGradientMax = mGradient[x][y];
-				}
-				
-				mDirection[x][y] = Math.atan2(gy, gx);
-			}
-		}
-	}
-	
-	/**
-	 * Normalizes gradient values to between 0 and 1
-	 */
-	public void normalizeGradient() {
-		if (mGradient != null) {			
-			for (int i=0; i < mSizeX; i++) {
-				for (int j=0; j < mSizeY; j++) {
-					if (mGradient[i][j] < mGradientMax * 0.2f) {
-						mGradient[i][j] = 0;
-					} else {
-						mGradient[i][j] = mGradient[i][j] / mGradientMax;
-					}
-				}
-			}
-		}
-	}
-	
-	public void quantize() {
-		mFeature = new double[64];
-		double maxQ = 0;
+		List<int[][]> kernels = new ArrayList<int[][]>();
+		kernels.add(KERNEL_0);
+		kernels.add(KERNEL_1);
+		kernels.add(KERNEL_2);
+		kernels.add(KERNEL_3);
 		
-		for (int i=0; i < mSizeX; i++) {
-			for (int j=0; j < mSizeY; j++) {
-				int qG = (int) Math.floor(mGradient[i][j] * 8); // 0..7
-				int qD = (int) Math.floor((mDirection[i][j] + Math.PI) / (2*Math.PI) * 8); // 0..7
-				//System.out.println("qG: " + qG + " qD: " + qD);
-				int index = qG + 8 * qD;
-				index = (index >= 64) ? 63 : index;
-				mFeature[index] += 1;
-				if (mFeature[index] > maxQ) {
-					maxQ = mFeature[index];
-				}
-			}
-		}
+		// first order feature extraction
+		List<double[][]> firstOrderList = new ArrayList<double[][]>();
+		for (int[][] kernel : kernels) {
+			double[][] firstOrder = new double[mSizeX][mSizeY];
 			
-		for (int k=0; k < 64; k++) {
-			mFeature[k] = mFeature[k] / maxQ;
+			for (x = 1; x < mSizeX - 1; x++) {
+				for (y = 1; y < mSizeY - 1; y++) {
+					double sum = 0;
+					
+					for (int i = -1; i <= 1; i++) {
+						for (int j = -1; j <= 1; j++) {
+							sum += kernel[i+1][j+1]
+									* mMatrix[x+i][y+j];
+						}
+					}
+					firstOrder[x][y] = sum < 0 ? -sum : sum; //rectify
+				}
+			}
+			firstOrderList.add(firstOrder);
+		}
+		
+		// second order feature extraction
+		mFeature = new double[kernels.size()*kernels.size()];
+		int fIndex = 0;
+		double base = 0;
+		for (double[][] firstOrder : firstOrderList) {
+			for (int[][] kernel : kernels) {
+				for (x = 1; x < mSizeX - 1; x++) {
+					for (y = 1; y < mSizeY - 1; y++) {
+						double sum = 0;
+						
+						for (int i = -1; i <= 1; i++) {
+							for (int j = -1; j <= 1; j++) {
+								sum += kernel[i+1][j+1]
+										* firstOrder[x+i][y+j];
+							}
+						}
+						
+						mFeature[fIndex] += sum < 0 ? -sum : sum; //rectify
+					}
+				}
+				base += mFeature[fIndex] * mFeature[fIndex];
+				fIndex ++;
+			}
+		}
+		
+		// normalize vector
+		base = Math.sqrt(base);
+		for (int i=0; i<fIndex; i++) {
+			mFeature[i] = mFeature[i] / base;
 		}
 	}
 	
@@ -122,24 +163,15 @@ public class SobelOperator implements FeatureExtractor {
 		return mSizeY;
 	}
 
-	public double[][] getGradient() {
-		return mGradient;
-	}
-
-	public double[][] getDirection() {
-		return mDirection;
-	}
-
 	@Override
 	public double[] getFeature(BufferedImage bi) {
 		this.mImage = bi;
 		this.mSizeX = this.mImage.getWidth();
 		this.mSizeY = this.mImage.getHeight();
 		
+		applyGaussianFilter();
 		compute();
-		normalizeGradient();
-		quantize();
-		
+
 		return mFeature;
 	}
 
