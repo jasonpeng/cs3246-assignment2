717940d590ef5edc26dfc39bc092c20c138baaea
jasonpeng <jasonpeng.boy@gmail.com>
2013-11-17 16:21:24 +0800

    updated index generator

--- test/cs3246/a2/test/GenerateImageIndex.java
+++ test/cs3246/a2/test/GenerateImageIndex.java
@@ -2,69 +2,68 @@ package cs3246.a2.test;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 
 import cs3246.a2.ColorCoherenceVector;
 import cs3246.a2.ColorHist;
-import cs3246.a2.Constant;
 import cs3246.a2.SobelOperator;
 import cs3246.a2.Util;
-import cs3246.a2.model.ImageIndex;
+import cs3246.a2.db.ImageIndexSqliteHelper;
 import cs3246.a2.model.ImageIndex;
 
 public class GenerateImageIndex {
 
-	public static void main(String[] args) {
-		List<ImageIndex> images = new ArrayList<ImageIndex>();
-		String IMAGE_FOLDER = args[0];
+	public static void main(String[] args) throws ClassNotFoundException {
+		ImageIndexSqliteHelper helper = new ImageIndexSqliteHelper("indexDB.sqlite");
+		helper.createTable();
+		
+		List<ImageIndex> indexList = new ArrayList<ImageIndex>();
+		String imageFolder = args[0];
+
 		try {
-			for (int i = 1; i <= 400; i++) {
-				try{
-					String filename = i + ".jpg";
-					System.out.println("Indexing " + filename);
-					
-					File file;
-					BufferedImage bi;
-					try{
-						file = new File(IMAGE_FOLDER+filename);
-						bi = ImageIO.read(file);
-						bi = Util.convertColorspace(bi, BufferedImage.TYPE_INT_RGB);
-					} catch (IIOException e){
-						filename = i + ".png";
-						System.out.println("File cannot found. Looking for: " + filename);
-						file = new File(IMAGE_FOLDER+filename);
-						bi = ImageIO.read(file);
-					}
+			File folder;
+			BufferedImage bi;
+			
+			folder = new File(imageFolder);
+			File[] imageFiles = folder.listFiles(new FilenameFilter() {
+			    public boolean accept(File dir, String name) {
+			    	String lowerName = name.toLowerCase();
+			    	boolean matched = lowerName.matches("^\\d+\\.(jpg|jpeg|png)$");
+			    	return matched;
+			    }
+			});
+			
+			for (File file : imageFiles) {
+				System.out.println("Indexing " + file.getName());
+				bi = ImageIO.read(file);
+				// convert image to 
+				bi = Util.convertColorspace(bi, BufferedImage.TYPE_INT_RGB);
+				
+				int id = Integer.parseInt(file.getName().split("\\.")[0]);
+				
+				ColorHist hist = new ColorHist();
+				double[] histogramResult = hist.getFeature(bi);
 	
-					ColorHist hist = new ColorHist();
-					double[] histogramResult = hist.getFeature(bi);
+				SobelOperator edge = new SobelOperator();
+				double[] edgeDirectionResult = edge.getFeature(bi);
 	
-					SobelOperator edge = new SobelOperator();
-					double[] edgeDirectionResult = edge.getFeature(bi);
+				ColorCoherenceVector ccv = new ColorCoherenceVector();
+				double[] ccvResult = ccv.getFeature(bi);
 	
-					ColorCoherenceVector ccv = new ColorCoherenceVector();
-					double[] ccvResult = ccv.getFeature(bi);
-	
-					ImageIndex image = new ImageIndex(filename, ccvResult, histogramResult,
-							edgeDirectionResult, "");
-					images.add(image);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				ImageIndex imageIndex = new ImageIndex(id, ccvResult, histogramResult,
+						edgeDirectionResult);
+				indexList.add(imageIndex);
 			}
 			
-			ImageDBWriter writer = new ImageDBWriter(images,
-					Constant.DB_FILE_NAME);
-			writer.write();
-			
+			helper.persist(indexList);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 }
