f69f7f0ad69c5c942af635264e38f2ebe94d6db6
Hu Qiang <nickhuq@gmail.com>
2013-11-17 21:31:39 +0800

    Added Command Class

---
+++ src/cs3246/a2/model/Command.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package cs3246.a2.model;
+
+/**
+ * @author huqiang
+ *
+ */
+public class Command {
+	public String commandType;
+	public boolean isCropped;
+	public int cropX;
+	public int cropY;
+	public int cropW;
+	public int cropH;
+	public String category;
+	public String queryImage;
+	public String queryColor;
+	public int numberOfResult;
+}
